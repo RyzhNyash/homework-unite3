3cc6bfd81aa484c11174b6ba9ed76aaa3f28a886
RyzhNyash <60988190+RyzhNyash@users.noreply.github.com>
2020-03-11 00:07:45 +0300

    Update TaskCurrencyConverter.java

--- TaskCurrencyConverter.java
+++ TaskCurrencyConverter.java
@@ -1,4 +1,4 @@
-package by.ld.unit03.main;
+package by.ld.unit3.main;
 
 import java.util.Scanner;
 
@@ -7,14 +7,16 @@ public class TaskCurrencyConverter {
 
 
         //        Scanner inData = new Scanner(System.in);
-        String type1 = inputType1();
-        String type2 = inputType2();
+        String type1 = inputType1(); //во что конвертируем
+        String type2 = inputType2(); //что конвертируем
         double sumIn = sumIn1();
+        double sumOut = 0;
+        System.out.println("__________");
 
         //        курс и виды валюты
         String rubS = "1";
         String dolS = "2";
-        String docS = "3";
+        String dolC = "3";
         String eurS = "4";
 
         double rubRu = 1; //значения относительно рубля
@@ -23,14 +25,39 @@ public class TaskCurrencyConverter {
         double eurEs = 0.013;
 
         //        конвертер
+        if ((type1 == rubS) & (type2 == dolS)) {
+            sumOut = sumIn * dolUs;
+            System.out.println(sumIn + " российских рублей = " + sumOut + " $ ");
+        } else if ((type1 == rubS) & (type2 == dolC)) {
+            sumOut = sumIn * dolCa;
+            System.out.println(sumIn + " российских рублей = " + sumOut + " канадских долларов");
+        } else if ((type1 == rubS) & (type2 == eurS)) {
+            sumOut = sumIn * eurEs;
+            System.out.println(sumIn + " российских рублей = " + sumOut + " евро ");
+        } else if ((type1 == dolC) & (type2 == rubS)) {
+            sumOut = sumIn / dolUs;
+            System.out.println(sumIn + " $ = " + sumOut + "российским рублям ");
+        } else if ((type1 == dolC) & (type2 == rubS)) {
+            sumOut = sumIn * dolCa;
+            System.out.println(sumIn + " канадских долларов = " + sumOut + "российским рублей ");
+        } else if ((type1 == eurS) & (type2 == rubS)) {
+            sumOut = sumIn / eurEs;
+            System.out.println(sumIn + " евро " + sumOut + " российским рублям ");
+        } else {
+            System.out.println("что то в логике твоей не верно");
+        }
 
+        for (int i = 1; i < 10; i++) {
+            double test = type1.length();
+            System.out.println(test);
+        }
 
         //        вывод
 
         System.out.println(type1);
         System.out.println(type2);
-        System.out.println(sumIn);
 
+//__________декомпозиция !!!
     }
 
     private static String inputType1() {
