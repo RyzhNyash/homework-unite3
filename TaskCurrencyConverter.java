cfbaa0edda63e78a0c867b93c8c6af73cbd7b2de
RyzhNyash <60988190+RyzhNyash@users.noreply.github.com>
2020-04-17 01:07:47 +0300

    Update TaskCurrencyConverter.java

--- TaskCurrencyConverter.java
+++ TaskCurrencyConverter.java
@@ -1,4 +1,4 @@
-package by.ld.unit3.main;
+package by.ld.unit3;
 
 import java.util.Scanner;
 
@@ -7,45 +7,63 @@ public class TaskCurrencyConverter {
 
 
         //        Scanner inData = new Scanner(System.in);
-        
-        String type1 = inputType1(); //во что конвертируем
+        String type1 = inputType1(); //в какую валюту конвертируем
         String type2 = inputType2(); //что конвертируем
-        double sumIn = sumIn1();
+        double sumIn = sumIn1(); // сумма к конвертации
         double sumOut = 0;
         System.out.println("__________");
 
         //        курс и виды валюты
-        
         String rubS = "1";
         String dolS = "2";
         String dolC = "3";
         String eurS = "4";
 
-        double rubRu = 1; //значения относительно рубля
-        double dolUs = 0.015;
-        double dolCa = 0.02;
-        double eurEs = 0.013;
+        //      коэффициенты для конверт.
+
+        double rubRu = 1; //остальный значения относительно рубля
+        double dolUs = 0.014;
+        double dolCa = 0.019;
+        double eurEu = 0.012;
 
         //        конвертер
-        
+
         if ((rubS.equals(type1)) && (dolS.equals(type2))) {
             sumOut = sumIn * dolUs;
             System.out.println(sumIn + " российских рублей = " + sumOut + " $ ");
         } else if ((type1.equals(rubS)) && (type2.equals(dolC))) {
             sumOut = sumIn * dolCa;
-            System.out.println(sumIn + " российских рублей = " + sumOut + " канадских долларов");
+            System.out.println(sumIn + " российских рублей = " + sumOut + " канадских $");
         } else if ((type1.equals(rubS)) && (type2.equals(eurS))) {
-            sumOut = sumIn * eurEs;
+            sumOut = sumIn * eurEu;
             System.out.println(sumIn + " российских рублей = " + sumOut + " евро ");
-        } else if ((type1.equals(dolC)) && (type2.equals(rubS))) {
+        } else if ((type1.equals(dolS)) && (type2.equals(rubS))) {
             sumOut = sumIn / dolUs;
-            System.out.println(sumIn + " $ = " + sumOut + "российским рублям ");
+            System.out.println(sumIn + " $ = " + sumOut + " российским рублям ");
         } else if ((type1.equals(dolC)) && (type2.equals(rubS))) {
-            sumOut = sumIn * dolCa;
-            System.out.println(sumIn + " канадских долларов = " + sumOut + "российским рублей ");
+            sumOut = sumIn / dolCa;
+            System.out.println(sumIn + " канадских $ = " + sumOut + "российским рублей ");
         } else if ((type1.equals(eurS)) && (type2.equals(rubS))) {
-            sumOut = sumIn / eurEs;
-            System.out.println(sumIn + " евро " + sumOut + " российским рублям ");
+            sumOut = sumIn / eurEu;
+            System.out.println(sumIn + " евро = " + sumOut + " российским рублям ");
+        } else if ((type1.equals(eurS)) && (type2.equals(dolS))) {
+            sumOut = (sumIn / dolUs) * eurEu;
+            System.out.println(sumIn + " евро = " + sumOut + " $ ");
+        } else if ((type1.equals(dolS)) && (type2.equals(eurS))) {
+            sumOut = (sumIn / eurEu) * dolUs;
+            System.out.println(sumIn + " $ = " + sumOut + " евро ");
+        } else if ((type1.equals(dolC)) && (type2.equals(dolS))) {
+            sumOut = (sumIn / dolCa) * dolUs;
+            System.out.println(sumIn + " канадский $ = " + sumOut + " $ ");
+        } else if ((type1.equals(dolS)) && (type2.equals(dolC))) {
+            sumOut = (sumIn / dolUs) * dolCa;
+            System.out.println(sumIn + " $ = " + sumOut + " канадский $ ");
+        } else if ((type1.equals(dolC)) && (type2.equals(eurS))) {
+            sumOut = (sumIn / dolCa) * eurEu;
+            System.out.println(sumIn + " канадский $ = " + sumOut + " евро ");
+        } else if ((type1.equals(eurS)) && (type2.equals(dolC))) {
+            sumOut = (sumIn / dolUs) * dolCa;
+            System.out.println(sumIn + " евро = " + sumOut + " канадский $ ");
         } else {
             System.out.println("что то в логике твоей не верно");
         }
@@ -53,11 +71,13 @@ public class TaskCurrencyConverter {
         //        вывод
 
         System.out.println("первый выбор = " + type1);
-        System.out.println("второй выбор = " + type2);       
+        System.out.println("второй выбор = " + type2);
+
+        return;
     }
 
-    // декомпозиция !!!
-    
+    //__________декомпозиция !!!
+
     private static String inputType1() {
 
         Scanner inData = new Scanner(System.in);
